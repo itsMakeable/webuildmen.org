afb7f93d650d1e84aaac80f7fb10da7eea5e0d11
Todd Lynch <todd@shifteight.com>
2014-09-16 08:24:39 -0400

    Toolkit initial Commit

--- js/zerif.js
+++ js/zerif.js
@@ -46,7 +46,7 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
 =================================== */
 
 $(document).ready(function() {
-  $('.main-nav-list').onePageNav({
+  $('#home .main-nav-list').onePageNav({
     scrollThreshold: 0.2, // Adjust if Navigation highlights too early or too late
     scrollOffset: 75, //Height of Navigation Bar
     changeHash: true
